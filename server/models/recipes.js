4724fa675cbebdede8baf031a4e2d49fce40d093
Olaide-EO <ebenezerie@yahoo.com>
2017-09-29 12:47:29 +0100

    Add Recipe Dummy data to test routing

---
+++ server/models/recipes.js
@@ -0,0 +1,48 @@
+const recipes = [
+    {
+        id: 1,
+        title: 'spiced vegetable chicken',
+        ingredients: ['chicken', 'vegetable', 'spices', 'onions', 'maggi', 'groundnut oil'],
+        description: 'Boil Chicken for 20 minutes, add maggi add onions, add spices learve to steam, fry for 5 minutes done.',
+        views: 98,
+        upvotes: 56,
+        downvotes: 67,
+        favorites: 98
+    },
+
+    { 
+        id: 2,
+        title: 'Africa porridge',
+        ingredients: ['yam', 'palmoil', 'maggi', 'onions', 'vegetables'],
+        description: 'boil water add yam, add maggi add palm oil and leave to steam',
+        views: 65,
+        upvotes: 25,
+        downvotes: 45,
+        favorites: 35
+    }
+
+    { 
+        id: 3,
+        title: 'Grilled Yam molash',
+        ingredients: ['yam', 'palmoil', 'maggi', 'molash', 'fish'],
+        description: 'boil water add yam, add maggi add palm oil and leave to steam',
+        views: 890,
+        upvotes: 353,
+        downvotes: 200,
+        favorites: 29
+    }
+
+    { 
+        id: 4,
+        title: 'Jollof Rice',
+        ingredients: ['rice', 'groundnut oul', 'chicken', 'onions', 'spices'],
+        description: 'boil water add rice, boil chiken add palm oil and leave to steam',
+        views: 890,
+        upvotes: 353,
+        downvotes: 200,
+        favorites: 29
+    }
+
+
+
+];
